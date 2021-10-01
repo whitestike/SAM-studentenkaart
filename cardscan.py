cc809bdcbb62a7a370fe2bf2fc420cee2da99a1b
whitestike <32246781+whitestike@users.noreply.github.com>
2021-10-01 09:30:27 +0200

    debug

--- cardscan.py
+++ cardscan.py
@@ -77,12 +77,10 @@ def main():
     if student == None:
         student = GetData(studentID, "teachers")
 
-<<<<<<< HEAD
-=======
     if student[2] + " " + student[3] in studentList:
         print("card already scanned")
         main()
->>>>>>> a297745263a5959220c00fdcc945123f347ac451
+
         print("welcome " + student[2] + " " + student[3])
     else:
         studentList.append(student[2] + " " + student[3])
