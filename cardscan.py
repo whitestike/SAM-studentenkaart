5e1147cc3464b9cf579c925315b057b778b262ea
whitestike <32246781+whitestike@users.noreply.github.com>
2021-10-01 11:52:17 +0200

    pusher

--- cardscan.py
+++ cardscan.py
@@ -3,8 +3,22 @@ from nfc.clf import RemoteTarget
 from time import sleep
 import secrets
 import pymysql
+import pusher
 
 studentList = []
+
+def InitPusher():
+
+    pusher_client = pusher.Pusher(
+    app_id='1275452',
+    key='92353ee8426715c5cc4f',
+    secret='c5e4a1a431e5d39cdc8d',
+    cluster='eu',
+    ssl=True
+    )
+
+    pusher_client.trigger('my-channel', 'my-event', {'message': 'hello world'})
+    
 
 def Scan():
     clf = nfc.ContactlessFrontend('usb')
